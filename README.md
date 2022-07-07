39b45310b0c3b80d67d84629aeb4c50bb8be9120
Your Name <gr8@nxt.ru>
2022-07-07 22:20:00 +0300

    deploying in devnet

---
+++ README.md
@@ -0,0 +1,6 @@
+# Solana Tic Tac Toe game
+
+Smart contract built with [Anchor](https://www.anchor-lang.com/) framework
+
+Deploy cost: ~3 SOL
+Testing: ~0.01 SOL
--- programs/solana-tic-tac-toe/src/lib.rs
+++ programs/solana-tic-tac-toe/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9163nkVWAADRHwSsK4NsbXEZ7UvqWXTR4inGDfustd5v");
 
 #[program]
 pub mod solana_tic_tac_toe {
